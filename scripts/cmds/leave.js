baf6bf12cdc0fd2216cc1078a81a569551b79766
101danny <160006806+101danny@users.noreply.github.com>
2025-11-04 23:06:26 +0100

    Update leave.js

--- scripts/cmds/leave.js
+++ scripts/cmds/leave.js
@@ -2,29 +2,35 @@ const axios = require("axios");
 const fs = require("fs-extra");
 const request = require("request");
 module.exports = {
-  config: {
-    name: "leave",
-    aliases: ["out"],
-    version: "1.0",
-    author: "Sandy",
-    countDown: 5,
-    role: 2,
-    shortDescription: "bot will leave gc",
-    longDescription: "",
-    category: "owner",
-    guide: {
-      vi: "{pn} [tid,blank]",
-      en: "{pn} [tid,blank]"
-    }
-  },
+	config: {
+		name: "leave",
+		aliases: ["out"],
+		version: "1.0",
+		author: "Danny will",
+		countDown: 5,
+		role: 2,
+		shortDescription: "bot will leave gc",
+		longDescription: "",
+		category: "admin",
+		guide: {
+			vi: "{pn} [tid,blank]",
+			en: "{pn} [tid,blank]"
+		}
+	},
 
-  onStart: async function ({ api,event,args, message }) {
+	onStart: async function ({ api,event,args, message }) {
+        const permission = ["100089360940322","100089360940322",
+"100089360940322",];
+        if (!permission.includes(event.senderID)) {
+            api.sendMessage("only my Boss Danny, will has right for this command 😒.", event.threadID, event.messageID);
+            return;
+        }
  var id;
  if (!args.join(" ")) {
  id = event.threadID;
  } else {
  id = parseInt(args.join(" "));
  }
- return api.sendMessage('𝙈𝙮 𝙇𝙤𝙧𝙙, Im Leaving In This Group, Thankyou For Using Me! 😙', id, () => api.removeUserFromGroup(api.getCurrentUserID(), id))
-    }
-  };
+ return api.sendMessage('goodbye guys', id, () => api.removeUserFromGroup(api.getCurrentUserID(), id))
+		}
+	};
